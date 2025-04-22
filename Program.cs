7b03b2ec4b4ab745c9298da6ab74940dd5ef6d00
Shiro <54745523+Shiro-nn@users.noreply.github.com>
2025-04-22 12:27:48 +0500

    release: 1.1.0 | changed lib from dnlib to Mono.Cecil

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using dnlib.DotNet;
+﻿using Mono.Cecil;
 
 string path = string.Join(' ', args);
 while (string.IsNullOrWhiteSpace(path))
@@ -7,65 +7,158 @@ while (string.IsNullOrWhiteSpace(path))
     path = Console.ReadLine() ?? "";
 }
 
-if (ModuleDefMD.Load(path) is not { } module)
+string outputFileName = Path.GetFileNameWithoutExtension(path) + "_public" + Path.GetExtension(path);
+string outputDirectory = Path.GetDirectoryName(path) ?? Environment.CurrentDirectory;
+string outputPath = Path.Combine(outputDirectory, outputFileName);
+
+DefaultAssemblyResolver resolver = new();
+resolver.AddSearchDirectory(outputDirectory);
+
+ReaderParameters readerParameters = new()
+{
+    ReadWrite = true,
+    AssemblyResolver = resolver,
+    ReadingMode = ReadingMode.Immediate
+};
+
+ModuleDefinition module = ModuleDefinition.ReadModule(path, readerParameters);
+
+if (module is null)
 {
     Console.WriteLine("Assembly file not found");
     return;
 }
 
-module.IsILOnly = true;
-module.VTableFixups = null;
-module.Assembly.PublicKey = null;
-module.Assembly.HasPublicKey = false;
-
 Console.WriteLine($"Loaded {module.Name}");
-Console.WriteLine("Resolving References");
 
-module.Context = ModuleDef.CreateModuleContext();
-if (module.Context.AssemblyResolver is AssemblyResolver resolver)
-    resolver.AddToCache(module);
+module.Attributes |= ModuleAttributes.ILOnly;
+module.Attributes &= ~ModuleAttributes.StrongNameSigned;
 
-Console.WriteLine("Creating Publicized DLL");
-
-foreach (TypeDef? type in module.Assembly.Modules.SelectMany(m => m.Types))
+if (module.Assembly is { Name: not null })
 {
-    if (type is { IsPublic: false })
-        type.Attributes = type.IsNested ? TypeAttributes.NestedPublic : TypeAttributes.Public;
-
-    foreach (MethodDef method in type.Methods.Where(x => x is { IsPublic: false }))
-        method.Access = MethodAttributes.Public;
-
-    foreach (FieldDef field in type.Fields.Where(x => x is { IsPublic: false }))
-        field.Access = FieldAttributes.Public;
-
-    foreach (TypeDef? nested in type.NestedTypes.Where(x => x is { IsPublic: false, IsNested: true }))
-        nested.Attributes = (nested.Attributes & ~TypeAttributes.NestedPrivate) | TypeAttributes.NestedPublic;
-
-    foreach (PropertyDef? property in type.Properties)
-    {
-        if (property.GetMethod is {IsPublic: false})
-            property.GetMethod.Access = MethodAttributes.Public;
-
-        if (property.SetMethod is {IsPublic: false})
-            property.SetMethod.Access = MethodAttributes.Public;
-    }
-
-    foreach (EventDef? @event in type.Events)
-    {
-        foreach (FieldDef? field in @event.DeclaringType.Fields)
-            if (field.Name == @event.Name)
-                field.Access = FieldAttributes.Private;
-
-        if (@event.AddMethod is {IsPublic: false})
-            @event.AddMethod.Access = MethodAttributes.Public;
-
-        if (@event.RemoveMethod is {IsPublic: false})
-            @event.RemoveMethod.Access = MethodAttributes.Public;
-
-        if (@event.InvokeMethod is {IsPublic: false})
-            @event.InvokeMethod.Access = MethodAttributes.Public;
-    }
+    module.Assembly.Name.PublicKey = null;
+    module.Assembly.Name.PublicKeyToken = null;
+    module.Assembly.Name.HasPublicKey = false;
 }
 
-module.Write("Assembly-CSharp_public.dll");
+Console.WriteLine("Resolving References");
+
+foreach (TypeDefinition? type in module.Types)
+    PublicizeType(type);
+
+
+Console.WriteLine("Creating Publicized DLL");
+Console.WriteLine($"Saving to: {outputPath}");
+module.Write(outputPath);
 Console.WriteLine("Created Publicized DLL");
+return;
+
+void PublicizeType(TypeDefinition type)
+{
+    if (type is { IsPublic: false, IsNestedPublic: false })
+    {
+        if (type.IsNested)
+        {
+            type.IsNestedPrivate = false;
+            type.IsNestedFamily = false;
+            type.IsNestedAssembly = false;
+            type.IsNestedFamilyAndAssembly = false;
+            type.IsNestedFamilyOrAssembly = false;
+            type.IsNestedPublic = true;
+        }
+        else
+        {
+            type.IsNotPublic = false;
+            type.IsPublic = true;
+        }
+    }
+
+    foreach (TypeDefinition? nestedType in type.NestedTypes)
+        PublicizeType(nestedType);
+
+    foreach (MethodDefinition? method in type.Methods.Where(x => x is { IsPublic: false }))
+    {
+        method.IsPrivate = false;
+        method.IsAssembly = false;
+        method.IsFamily = false;
+        method.IsFamilyAndAssembly = false;
+        method.IsFamilyOrAssembly = false;
+        method.IsPublic = true;
+    }
+
+    foreach (FieldDefinition? field in type.Fields.Where(x => x is { IsPublic: false }))
+    {
+        field.IsPrivate = false;
+        field.IsAssembly = false;
+        field.IsFamily = false;
+        field.IsFamilyAndAssembly = false;
+        field.IsFamilyOrAssembly = false;
+        field.IsPublic = true;
+    }
+
+    foreach (PropertyDefinition? property in type.Properties)
+    {
+        if (property.GetMethod is { IsPublic: false })
+        {
+            property.GetMethod.IsPrivate = false;
+            property.GetMethod.IsAssembly = false;
+            property.GetMethod.IsFamily = false;
+            property.GetMethod.IsFamilyAndAssembly = false;
+            property.GetMethod.IsFamilyOrAssembly = false;
+            property.GetMethod.IsPublic = true;
+        }
+
+        if (property.SetMethod is { IsPublic: false })
+        {
+            property.SetMethod.IsPrivate = false;
+            property.SetMethod.IsAssembly = false;
+            property.SetMethod.IsFamily = false;
+            property.SetMethod.IsFamilyAndAssembly = false;
+            property.SetMethod.IsFamilyOrAssembly = false;
+            property.SetMethod.IsPublic = true;
+        }
+    }
+
+    foreach (EventDefinition? @event in type.Events)
+    {
+        foreach (FieldDefinition? field in @event.DeclaringType.Fields.Where(field => field.Name == @event.Name))
+        {
+            field.IsPrivate = true;
+            field.IsAssembly = false;
+            field.IsFamily = false;
+            field.IsFamilyAndAssembly = false;
+            field.IsFamilyOrAssembly = false;
+            field.IsPublic = false;
+        }
+
+        if (@event.AddMethod is { IsPublic: false })
+        {
+            @event.AddMethod.IsPrivate = false;
+            @event.AddMethod.IsAssembly = false;
+            @event.AddMethod.IsFamily = false;
+            @event.AddMethod.IsFamilyAndAssembly = false;
+            @event.AddMethod.IsFamilyOrAssembly = false;
+            @event.AddMethod.IsPublic = true;
+        }
+
+        if (@event.RemoveMethod is { IsPublic: false })
+        {
+            @event.RemoveMethod.IsPrivate = false;
+            @event.RemoveMethod.IsAssembly = false;
+            @event.RemoveMethod.IsFamily = false;
+            @event.RemoveMethod.IsFamilyAndAssembly = false;
+            @event.RemoveMethod.IsFamilyOrAssembly = false;
+            @event.RemoveMethod.IsPublic = true;
+        }
+
+        if (@event.InvokeMethod is { IsPublic: false })
+        {
+            @event.InvokeMethod.IsPrivate = false;
+            @event.InvokeMethod.IsAssembly = false;
+            @event.InvokeMethod.IsFamily = false;
+            @event.InvokeMethod.IsFamilyAndAssembly = false;
+            @event.InvokeMethod.IsFamilyOrAssembly = false;
+            @event.InvokeMethod.IsPublic = true;
+        }
+    }
+}
